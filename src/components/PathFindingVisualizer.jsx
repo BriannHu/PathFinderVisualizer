db8fcf069adbfa45216bed1feb5d72ced0305319
Brian Hu <blp.hu5@gmail.com>
2021-05-22 10:44:51 -0400

    initial commit

---
+++ src/components/PathFindingVisualizer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class PathFindingVisualizer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>Hello world
+
+            </div>
+        )
+    }
+}
+
+export default PathFindingVisualizer;
